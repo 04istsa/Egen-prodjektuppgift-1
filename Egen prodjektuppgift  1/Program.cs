58ba955de4556aa87b4610767250916658369933
Isak Tschährä <04ISTSA@skola.lidkoping.se>
2021-10-11 15:52:19 +0200

    Add project files.

---
+++ Egen prodjektuppgift  1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Egen_prodjektuppgift__1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hej duska gissa på ett tal mellan ett och tio");
+            int gues = int.Parse(Console.ReadLine());
+            int randNum = rand.Next(1, 11);
+            
+            if (gues == randNum)
+            {
+                Console.WriteLine("Grattis du gissad rätt");
+            } else 
+            { 
+
+            while(gues != randNum)
+            {
+
+            }
+            
+            }
+        }
+    }
+}
